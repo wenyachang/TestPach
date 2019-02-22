d2fe74ab7ba13faa211cb9a60ecabfa572bdbc9e
wenyachang <wenyachang@163.com>
2019-02-22 12:02:33 +0800

    delete

--- pach/pack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh  
-exe="test" #你需要发布的程序名称
-des="/home/wenya/wenya/project/pach" #步骤1中的目录即本文件所在目录
-deplist=$(ldd $exe | awk  '{if (match($3,"/")){ printf("%s "),$3 } }')  
-cp $deplist $des
